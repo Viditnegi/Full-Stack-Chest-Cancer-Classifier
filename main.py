9f4560b81e2d978074e244645d5de9d190db9038
Vidit <viditsn@gmail.com>
2024-05-26 15:38:13 +0530

    data_ingestion_done

--- main.py
+++ main.py
@@ -1,3 +1,17 @@
 from chest_cancer_classifier import logger
+from chest_cancer_classifier.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 
-logger.info("Welcome to cnnClassifier!")
+
+
+
+STAGE_NAME = "Data Ingestion stage"
+print(STAGE_NAME)
+
+try:
+    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+    obj = DataIngestionTrainingPipeline()
+    obj.main()
+    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+    logger.exception(e)
+    raise e
---
+++ src/chest_cancer_classifier/constants/__init__.py
@@ -0,0 +1,6 @@
+from pathlib import Path
+import os
+
+print(os.getcwd())
+CONFIG_FILE_PATH = Path("config/config.yaml")
+PARAMS_FILE_PATH = Path("params.yaml")
